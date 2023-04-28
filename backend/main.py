0866c994b2eaa299b534664f47a9d1f8cd715a83
Gracjan P. W. Chudziak <gracjanchudziak@gmail.com>
2023-04-28 03:47:16 +0200

    feat: Add origins

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from core.endpoints import quiz, queue, cookie, chart
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(quiz.router)
 app.include_router(queue.router)
