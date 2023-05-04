5eec0fd98b7beb5d9c058870b18f5ce1723981a9
Gracjan P. W. Chudziak <gracjanchudziak@gmail.com>
2023-05-04 02:49:18 +0200

    fix: Change path for db

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -18,7 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from backend.core.models.models import Base
+from core.models.models import Base
 
 target_metadata = Base.metadata
 
