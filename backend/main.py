0fffe6957fa989bcfdc4a51679b78d6061c78581
Gracjan P. W. Chudziak <gracjanchudziak@gmail.com>
2023-05-11 04:08:34 +0200

    refactor: Delete cookie endpoint and change origins

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from core.endpoints import quiz, queue, cookie, chart
+from core.endpoints import quiz, queue, chart
 
 app = FastAPI()
 
-origins = [
-    "http://127.0.0.1:5173",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -19,5 +15,4 @@ app.add_middleware(
 
 app.include_router(quiz.router)
 app.include_router(queue.router)
-app.include_router(cookie.router)
 app.include_router(chart.router)
